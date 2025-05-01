d968d2d58d3dbd607cea53cfe2675095822a6abf
iulial <iuliamarialungu@yahoo.com>
2025-05-01 12:29:45 +0200

    restayled Footer component for improved layout and styling

--- frontend/src/components/ui/Footer.tsx
+++ frontend/src/components/ui/Footer.tsx
@@ -3,21 +3,19 @@ import { BrandLogo } from "./BrandLogo";
 import { Link } from "react-router-dom";
 
 export const Footer: React.FC = () => (
-  <footer className="w-full bg-slate-50 border-t border-slate-200 py-4 px-6 flex flex-col gap-2 items-center justify-between mt-12">
-    <div className="w-full flex items-center justify-between">
-      <div>
+  <footer className="w-full bg-slate-50 border-t border-slate-200 py-4 px-6 flex flex-col gap-2 mt-12">
+    <div className="self-end text-right">
+      <BrandLogo className="-mb-4 -mr-.5"/>
+      <br />
         <Link
           to={process.env.REACT_APP_PRIVACY_POLICY_URL || "/privacy"}
-          className="text-blue-500 hover:text-blue-600 text-sm transition-colors"
+          className="underline block text-blue-500 hover:text-blue-600 text-sm transition-colors"
         >
           Privacy Policy
         </Link>
-      </div>
-      <div className="flex items-center mr-1">
-        <BrandLogo />
-      </div>
     </div>
-    <div className="w-full text-right text-xs text-slate-400 mt-1 pr-1">
+
+    <div className="w-full text-right text-xs text-slate-400 mt-1">
       © {new Date().getFullYear()} LabsExplained. All rights reserved.
     </div>
   </footer>
